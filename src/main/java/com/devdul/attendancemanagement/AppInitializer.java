84783bbe3ef68c538fcb64033ad226c3d3df4859
iamdul <kalharidulanga@gmail.com>
2024-02-15 18:24:00 +0530

    design the login page

--- src/main/java/com/devdul/attendancemanagement/AppInitializer.java
+++ src/main/java/com/devdul/attendancemanagement/AppInitializer.java
@@ -10,9 +10,9 @@ import java.io.IOException;
 public class AppInitializer extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(AppInitializer.class.getResource("View/hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(AppInitializer.class.getResource("View/LoginForm.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Attendance Management System");
         stage.setScene(scene);
         stage.show();
     }
---
+++ src/main/java/com/devdul/attendancemanagement/Controller/LoginFormController.java
@@ -0,0 +1,21 @@
+package com.devdul.attendancemanagement.Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class LoginFormController {
+
+    public TextField txtUsername;
+    public AnchorPane context;
+    public PasswordField txtPassword;
+
+    public void studentOnAction(ActionEvent actionEvent) {
+    }
+
+    public void adminOnAction(ActionEvent actionEvent) {
+    }
+}
